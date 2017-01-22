95375c407787730d6699390c4dac6cbbadf3636a
Bastian Machek <bastian@machek.inf>
2017-01-22 09:20:22 +0100

    JSON Aufruf mit Parameter funktioniert jetz.

--- app/src/main/java/systems/machek/tankdroid/LiveCockpit.java
+++ app/src/main/java/systems/machek/tankdroid/LiveCockpit.java
@@ -9,7 +9,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import systems.machek.tankdroid.systems.machek.tankdroid.network.JsonSender;
 import systems.machek.tankdroid.widgets.MjpegFragment;
 
 public class LiveCockpit extends AppCompatActivity {
@@ -66,5 +72,43 @@ public class LiveCockpit extends AppCompatActivity {
             }
             return null;
         }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        return true;
+    }
+
+    @Override
+    public boolean dispatchGenericMotionEvent(MotionEvent ev) {
+
+        String ip = getSharedPreferences(Constants.PREF_FILE_NAME, Context.MODE_PRIVATE).getString(Constants.CONFIG_IP_ADDRESS, Constants.DEFAULT_IP_ADDRESS);
+
+        float throttleLeft = ev.getAxisValue(MotionEvent.AXIS_Y);
+        float throttleRight = ev.getAxisValue(MotionEvent.AXIS_RZ);
+
+
+        JSONObject json = new JSONObject();
+
+        try {
+            json.put("method", "move");
+            JSONObject args = new JSONObject();
+            args.put("move_l", throttleLeft);
+            args.put("move_r", throttleRight);
+            args.put("move_t", 0.0);
+            json.put("params", args);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        JsonSender command = new JsonSender(ip, json.toString());
+        Thread t = new Thread(command);
+        t.start();
+
+
+
+        return true;
     }
 }
